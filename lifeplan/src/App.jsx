9c6280ae35a3bb2a8de51af1cc119b70c76adb57
Dev Madlani <madlanidev@gmail.com>
2024-09-22 00:47:18 +0530

    video

---
+++ lifeplan/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoRecorder from './components/VideoRecorder';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React IndexedDB Video Storage</h1>
+      <VideoRecorder />
+    </div>
+  );
+}
+
+export default App;
